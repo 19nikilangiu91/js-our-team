c951c938131254e0f279e680f9b8aed3f0af39c6
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-21 13:47:58 +0100

    Milestone 1 Done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 // Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
 // Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+
 // MILESTONE 0:
 // Creare l’array di oggetti con le informazioni fornite.
-
 const membriTeam = [
     {
         "nome" : "Wayne Barnett",
@@ -37,3 +38,14 @@ const membriTeam = [
 ];
 
 console.log(membriTeam);
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto.
+
+// Andiamo a creare un ciclo for
+
+for(let i = 0; i < membriTeam.length; i++){
+
+    let membroIesimo = membriTeam[i];
+    console.log(membroIesimo);
+}
