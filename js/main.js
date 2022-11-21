b143478e2b2f2c3d3be27f3c612fcca744023667
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-21 13:41:55 +0100

    Punti 1 e 2 e Milestone 0 Done

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+const membriTeam = [
+    {
+        "nome" : "Wayne Barnett",
+        "role" : " Founder & CEO",
+        "image" : "image",
+    },
+    {
+        "nome" : "Angela Caroll",
+        "role" : "Chief Editor",
+        "image" : "image",
+    },
+    {
+        "nome" : "Walter Gordon",
+        "role" : "Office Manager",
+        "image" : "image",
+    },
+    {
+        "nome" : "Angela Lopez",
+        "role" : "Social Media Manager",
+        "image" : "image",
+    },
+    {
+        "nome" : "Scott Estrada",
+        "role" : "Developer",
+        "image" : "image",
+    },
+    {
+        "nome" : "Barbara Ramos",
+        "role" : "Graphic Designer",
+        "image" : "image",
+    },
+];
+
+console.log(membriTeam);
